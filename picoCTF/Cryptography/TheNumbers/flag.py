b68fd0ef4bd89d8bc9fad8c7b1306216b0629ef2
XD3an <deanhs0dean@gmail.com>
2022-08-12 05:22:40 -0400

    modify TheNumbers

---
+++ picoCTF/Cryptography/TheNumbers/flag.py
@@ -0,0 +1,16 @@
+import string
+
+
+flag_enc = [16, 9, 3, 15, 3, 20, 6,'{', 20, 8, 5, 14, 21, 13, 2, 5, 18, 19, 13, 1, 19, 15, 14, '}']
+
+case = string.ascii_uppercase
+
+flag = ""
+# print(flag_enc)
+
+for i in range(len(flag_enc)):
+    if flag_enc[i] == '{' or flag_enc[i] == '}':
+        flag += flag_enc[i]
+    else:
+        flag += case[flag_enc[i]-1]
+print(flag)
